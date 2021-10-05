201739baf4cd013cacdbdb62000faf6750d35741
shimiken0404 <shimizu.kenta0404@gmail.com>
2021-10-05 11:55:38 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :contributor_confirmation, only: [:edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update, :dastroy]
 
   def index
      @item = Item.order(created_at: :desc)
@@ -31,6 +31,13 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+     @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
