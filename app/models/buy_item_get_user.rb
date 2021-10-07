8a7ebf623f1b9d87c77d66f83e6100a846048632
shimiken0404 <shimizu.kenta0404@gmail.com>
2021-10-07 20:21:27 +0900

    formオブジェクト

---
+++ app/models/buy_item_get_user.rb
@@ -0,0 +1,20 @@
+class BuyItemGetUser
+  include ActiveModel::Model
+  attr_accessor :user, :item, :postal_code, :delivery_address_id, :address_city, :address_number, :address_building, :telephone_number
+
+  # get_user
+  with_options presence: true do
+    validates :postal_code,         format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :delivery_address_id, numericality: { other_than: 0 , message: "can't be blank"}
+    validates :address_city
+    validates :address_number
+    validates :telephone_number
+  end
+
+  def save #buy_item?
+
+    buy_item = BuyItem.create(item: item, user_id: user_id)
+
+    GetUser.create(postal_code: postal_code, delivery_address_id: delivery_address_id, address_city: address_city, address_number: address_number, telephone_number: telephone_number, buy_item_id: buy_item.id)
+  end
+end
