1ac4bf8b2f5bacf8d339b14fe0bc36bdafcd538a
shimiken0404 <shimizu.kenta0404@gmail.com>
2021-10-12 12:52:32 +0900

    クレジットカード機能の実装

--- app/controllers/get_users_controller.rb
+++ app/controllers/get_users_controller.rb
@@ -6,9 +6,12 @@ class GetUsersController < ApplicationController
   end
 
   def create
+
     @item = Item.find(params[:item_id])
     @buy_item_get_user = BuyItemGetUser.new(get_user_params)
+binding.pry
     if @buy_item_get_user.valid?
+      # pay_item
       @buy_item_get_user.save
       redirect_to root_path
     else  
@@ -19,6 +22,15 @@ class GetUsersController < ApplicationController
   private
 
   def get_user_params
-    params.permit(:postal_code, :delivery_address_id, :address_city, :address_number, :telephone_number).merge(user: current_user.id)
+    params.require(:buy_item_get_user).permit(:postal_code, :delivery_address_id, :address_city, :address_number, :telephone_number).merge(user: current_user.id, token: params[:token], item: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+    amount: @item.pay,   # 商品の値段
+    card: get_user_params[:token],    # カードトークン
+    currency: 'jpy'                   # 通貨の種類（日本円）
+  )
   end
 end
